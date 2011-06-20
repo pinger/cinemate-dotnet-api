6e2600edeb565aa155c6967a0fe5d2daeba25789
alexander.panfilenok@gmail.com <alexander.panfilenok@gmail.com@67641316-d3c8-8c89-24d9-cb2ffd1a8dbd>
2011-06-20 06:56:20 +0000

    Creating Project Files

---
+++ trunk/CinemateAPI/Common/CinematePoster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CinemateAPI.Common
+{
+	[Serializable]
+	public class CinematePoster
+	{
+		[XmlElement("small")] 
+		public Link Small;
+
+		[XmlElement("medium")] 
+		public Link Medium;
+
+		[XmlElement("big")] 
+		public Link Big;
+	}
+}
