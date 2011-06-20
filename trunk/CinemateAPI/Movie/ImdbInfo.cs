e437994d23ed82a2c0c2fd2cb267cf82646c13dd
alexander.panfilenok@gmail.com <alexander.panfilenok@gmail.com@67641316-d3c8-8c89-24d9-cb2ffd1a8dbd>
2011-06-20 06:56:30 +0000

    Creating Project Files

---
+++ trunk/CinemateAPI/Movie/ImdbInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CinemateAPI.Movie
+{
+	[Serializable]
+	public class ImdbInfo
+	{
+		[XmlAttribute("rating")] 
+		public float Rating;
+
+		[XmlAttribute("votes")] 
+		public int Votes;
+
+		public override string ToString()
+		{
+			return string.Format("Rating: {0}, Votes: {1}", Rating, Votes);
+		}
+	}
+}
