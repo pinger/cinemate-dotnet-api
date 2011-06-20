fea1930776a69e25279cafe0bae7b360eead3e84
alexander.panfilenok@gmail.com <alexander.panfilenok@gmail.com@67641316-d3c8-8c89-24d9-cb2ffd1a8dbd>
2011-06-20 06:57:39 +0000

    Creating Project Files

---
+++ trunk/CinemateAPI/CinemateCountry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CinemateAPI
+{
+	[Serializable]
+	public class CinemateCountry
+	{
+		[XmlElement("name")]
+		public string Name { get; set; }
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
